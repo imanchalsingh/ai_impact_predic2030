5f5088d25ae3c5f151028ee6076199266c93c5b5
imanchalsingh <singhanchal159357@gmail.com>
2026-06-02 15:12:49 +0530

    feat: add SQL, notebook, and visualization files for AI job impact analysis

--- analyzing_with.sql
+++ analyzing_with.sql
@@ -2,7 +2,27 @@ CREATE DATABASE `ai_impact_data`;
 
 USE `ai_impact_data`;
 
-SELECT * FROM `ai_impact_data`
-LIMIT 5;
+SELECT
+    *
+FROM
+    `ai_impact_data`
+LIMIT
+    5;
 
-SELECT COUNT(*) AS Total_Employees FROM `ai_impact_data`;
+SELECT
+    COUNT(*) AS Total_Employees
+FROM
+    `ai_impact_data`;
+
+SELECT
+    Job_Title,
+    MAX(AI_Replacement_Risk),
+    COUNT(Education_Level),
+    Education_Level
+FROM
+    `ai_impact_data`
+GROUP BY
+    Job_Title,
+    Education_Level
+ORDER BY
+    MAX(AI_Replacement_Risk) DESC;
